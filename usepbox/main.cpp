720bfbc59970c2a14dae15f8c837a46136ce6772
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-02-02 17:55:16 +0800

    update example to use libpbox

--- usepbox/main.cpp
+++ usepbox/main.cpp
@@ -1,8 +1,14 @@
 #include "foo.h"
 #include <iostream>
+#include <stdio.h>
 #include <vector>
 
 #define JSONFILE "/home/rasmus/src/ccbox/usepbox/test.json"
+
+//#define USE_DEMO1
+#define USE_DEMO2
+
+void test();
 
 void mytest(const std::vector<std::string>& k)
 {
@@ -22,21 +28,83 @@ void mytest(const std::vector<std::string>& k)
 #endif
 }
 
-int main()
+void print_banner(const char* msg)
 {
+    const int repeat = 20;
+    for (int i = 0; i < repeat; ++i) {
+        printf("=");
+    }
+    printf(" %s ", msg);
+    for (int i = 0; i < repeat; ++i) {
+        printf("=");
+    }
+
+    printf("\n");
+}
+
+int demo1()
+{
+#ifdef USE_DEMO1
     using namespace std;
 
+    print_banner("dump_jsonfile");
     dump_jsonfile(JSONFILE);
-#if 1
+    print_banner("get_vector_from_jsonfile");
     vector<string> k;
+    // .test
     k.push_back("test");
     mytest(k);
 
+    print_banner("get_vector_from_jsonfile");
+    // .query.result.item
     k.clear();
     k.push_back("query");
     k.push_back("result");
     k.push_back("item");
     mytest(k);
+
+    print_banner("get_double_from_jsonfile");
+    // .values.pi
+    k.clear();
+    k.push_back("values");
+    k.push_back("pi");
+    printf("double from .values.pi: %lf\n",
+           get_double_from_jsonfile(JSONFILE, k));
 #endif
+    return 0;
+}
+
+void demo2()
+{
+    //test();
+
+#ifdef USE_DEMO2
+    using namespace std;
+    vector<string> k;
+
+    print_banner("get_value_from_jsonfile");
+    // .values.pi
+    k.clear();
+    k.push_back("values");
+    k.push_back("pi");
+    double dd;
+    // if ( get_value_from_jsonfile(JSONFILE, k, dd) ) {
+    //     printf("float from .values.pi: %lf\n", dd);
+    // } else {
+    //     printf("failed to read value\n");
+    // }
+    dd = get_double_from_jsonfile(JSONFILE, k);
+    cout << dd << endl;
+
+#endif  // USE_DEMO2
+
+
+}
+
+int main()
+{
+    demo1();
+    demo2();
+
     return 0;
 }
