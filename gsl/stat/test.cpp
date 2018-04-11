441573fb464402e052ab7d4830f97f02203cd622
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-04-11 10:31:12 +0800

    update stat, use gaussian and uniform distribution to generate random data for practice

--- gsl/stat/test.cpp
+++ gsl/stat/test.cpp
@@ -6,6 +6,8 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 
+const size_t stride = 1;
+
 int mycompare(const void* a, const void* b)
 {
     double m = *(double*)a;
@@ -19,7 +21,7 @@ int mycompare(const void* a, const void* b)
     }
 }
 
-void fill_rng(double *buffer, size_t n)
+void fill_gauss_rng(double *buffer, size_t n)
 {
     const gsl_rng_type* T;
     gsl_rng* r;
@@ -27,8 +29,8 @@ void fill_rng(double *buffer, size_t n)
 
     gsl_rng_env_setup();
 
-    //T = gsl_rng_default;
-    T = gsl_rng_mt19937;
+    T = gsl_rng_default;
+    //T = gsl_rng_mt19937;
     r = gsl_rng_alloc(T);
 
     for (size_t i = 0; i < n; i++)
@@ -42,6 +44,20 @@ void fill_rng(double *buffer, size_t n)
     gsl_rng_free (r);
 }
 
+void fill_uniform(double* buffer, size_t n)
+{
+    const gsl_rng_type* T;
+    gsl_rng* r;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc(T);
+    for (size_t i = 0; i < n; ++i) {
+        double u = gsl_rng_uniform(r);
+        buffer[i] = u * 100.0;
+    }
+    gsl_rng_free(r);
+}
+
 void dump(double* arr, size_t n)
 {
     for (size_t i = 0; i < n; i++) {
@@ -50,27 +66,21 @@ void dump(double* arr, size_t n)
     printf("\n");
 }
 
-void test()
+void test_stat(double* arr, size_t size)
 {
-    const size_t size = 1000;
-    double vals[size];
-    double sorted[size];
-    const size_t stride = 1;
-
-    fill_rng(vals, size);
-
-    double mean = gsl_stats_mean(vals, stride, size);
-    double varn = gsl_stats_variance(vals, stride, size);
-    double varm = gsl_stats_variance_m(vals, stride, size, mean);
+    double mean = gsl_stats_mean(arr, stride, size);
+    double varn = gsl_stats_variance(arr, stride, size);
+    double varm = gsl_stats_variance_m(arr, stride, size, mean);
     double _min, _max;
-    gsl_stats_minmax(&_min, &_max, vals, stride, size);
+    gsl_stats_minmax(&_min, &_max, arr, stride, size);
 
     printf("mean: %.3f, varn: %.3f, varm: %.3f\n",
         mean, varn, varm);
     printf("min: %.3f, max: %.3f\n", _min, _max);
 
+    double* sorted = (double*)malloc(size*sizeof(double));
     // copy vals to sorted
-    memcpy(sorted, vals, size*sizeof(double));
+    memcpy(sorted, arr, size*sizeof(double));
     //printf("vals:\n");
     //dump(vals, size);
     //printf("sorted:\n");
@@ -79,5 +89,21 @@ void test()
 
     //printf("after qsort()\n");
     //dump(sorted, size);
-    printf("0: %.3f, last: %.3f\n", sorted[0], sorted[size-1]);
+    printf("sorted: 0: %.3f, -1: %.3f\n", sorted[0], sorted[size-1]);
+
+    printf("gsl_stats_sd: %.3f\n", gsl_stats_sd(arr, stride, size));
+    printf("gsl_stats_median_from_sorted_data: %.3f\n",
+        gsl_stats_median_from_sorted_data(sorted, 1, size));
+}
+
+void test()
+{
+    const size_t size = 1000;
+    double vals[size];
+
+    fill_gauss_rng(vals, size);
+    test_stat(vals, size);
+    printf("\n");
+    fill_uniform(vals, size);
+    test_stat(vals, size);
 }
