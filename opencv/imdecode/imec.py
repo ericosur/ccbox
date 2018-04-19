789b4267ade422b725a667e4406fe8db60b35dee
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-04-19 16:31:36 +0800

    update imdecode python script, using numpy to translate result of imencode
    Result of cv2.imencode is a byte array of 'jpg' or 'png' from
    a cv::Mat. Using numpy.array() to represent it as hex string.

--- opencv/imdecode/imec.py
+++ opencv/imdecode/imec.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python2
+# coding=utf-8
+
+# refer to http://cctg.blogspot.tw/2017/07/socket-opencv-client.html
 
 import cv2
+import numpy
 
 ifn = 'lena.jpg'
+ofn = 'out.jpg'
+tfn = 'out.txt'
 img = cv2.imread(ifn)
-#cv2.imshow("img", img)
-#cv2.waitKey(0)
 
-img_str = cv2.imencode('.jpg', img)[1].tostring()
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+result, img_str = cv2.imencode('.jpg', img, encode_param)
+#result, img_str = cv2.imencode('.jpg', img)
+#print('img_str: {}'.format(img_str))
+data = numpy.array(img_str)
+stringData = data.tostring()
 
-ofn = 'out.txt'
-ofile = open(ofn, "wb")
-ofile.write(img_str)
-ofile.flush()
-ofile.close()
+tfile = open(tfn, "w")
+tfile.write(stringData)
+tfile.close()
 
 # now read from file and decode into image
-rfile = open(ofn, "rb")
+rfile = open(tfn, "r")
 img_str2 = rfile.read()
+data = numpy.fromstring(img_str2, dtype='uint8')
+decimg = cv2.imdecode(data, 1)
 if len(img_str) == len(img_str2):
     print('size is same')
 else:
     print('size not matched')
     exit()
 
-readbackimg = cv2.imdecode(rfile, 0)
-cv2.imshow("readback", readbackimg)
-cv2.waitKey(0)
-
+cv2.imwrite(ofn, decimg)
+#cv2.imshow("readback", readbackimg)
+#cv2.waitKey(0)
