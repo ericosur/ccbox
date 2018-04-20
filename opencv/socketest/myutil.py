3096b013814984d843b780091575b37de4ac18b8
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-04-20 18:48:08 +0800

    update myutil for combine two images
    * using numpy concat function need same height if connect
      horizontally

--- opencv/socketest/myutil.py
+++ opencv/socketest/myutil.py
@@ -17,9 +17,29 @@ def translate_img_to_str(fn):
     stringData = data.tostring()
     return stringData
 
+def combine_two_images(ofn, inf1, inf2):
+    img1 = cv2.imread(inf1)
+    img2 = cv2.imread(inf2)
+
+    new_height = max(img1.shape[0], img2.shape[0])
+    new_width = max(img1.shape[1], img2.shape[1])
+    if img1.shape[0] > img2.shape[0]:
+        new_img = cv2.resize(img2, (new_height, new_width))
+        img2 = new_img
+    elif img1.shape[0] < img2.shape[0]:
+        new_img = cv2.resize(img1, (new_height, new_width))
+        img1 = new_img
+
+    # axis = 1, left is img1, and right is img2
+    # need have same height
+    vis = numpy.concatenate((img1, img2), axis=1)
+    cv2.imwrite(ofn, vis)
+
+
 def main():
-    strData = translate_img_to_str('lena.jpg')
-    print strData
+    # strData = translate_img_to_str('lena.jpg')
+    # print strData
+    combine_two_images('out.jpg', 'l128.jpg', 'l200.jpg')
 
 if __name__ == '__main__':
     main()
