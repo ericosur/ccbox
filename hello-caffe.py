eacc4948faead4f6d49b95f69dd98a6fff542506
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-09-06 10:51:29 +0800

    update hello-caffe.py, more description

--- hello-caffe.py
+++ hello-caffe.py
@@ -2,31 +2,41 @@ import os, sys
 
 def hello_world():
     import caffe
-    print 'hello world'
+    print('pycaffe version: {}'.format(caffe.__version__))
 
 def insert_syspath(p):
     if is_path_exist(p):
         print('insert {} into sys path'.format(p))
         sys.path.insert(0, p)
-    return
+        return True
+    return False
 
 def is_path_exist(p):
     return os.path.exists(p)
 
-def main():
+def init():
     env_caffe_root = os.environ['CAFFE_ROOT']
     pycaffe_path = '{}/python'.format(env_caffe_root)
-    if is_path_exist(pycaffe_path):
-        insert_syspath(pycaffe_path)
-        return
+    if insert_syspath(pycaffe_path):
+        print('insert syspath successfully'.format(pycaffe_path))
+        return True
 
+    #print('maybe {} not found?'.format(pycaffe_path))
     home_path = os.environ['HOME']
     #try_path = 'src/caffe-try'
     intel_path = 'src/github/intel/caffe'
     pycaffe_path = '{}/{}/python'.format(home_path,intel_path)
-    insert_syspath(caffe_root)
-    return
+    if insert_syspath(pycaffe_path):
+        print('2nd try insert syspath successfully'.format(pycaffe_path))
+        return True
 
+    return False
+
+def main():
+    if init():
+        hello_world()
+    else:
+        print('failed to import pycaffe...')
 
 if __name__ == '__main__':
     main()
