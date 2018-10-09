701453bf802275561933faf2b8b61d3b0c56526b
Rasmus_Lai <rasmus_lai@pegatroncorp.com>
2018-10-09 18:13:29 +0800

    UPDATE sample for transforming 2D pixel to 3D points

---
+++ rs/sample/dist.cpp
@@ -0,0 +1,48 @@
+#include "dist.h"
+
+void display_point(float point[3])
+{
+    cout << "(" << point[0] << "," << point[1] << "," << point[2] << ")\n";
+}
+
+float dist_3d(const rs2::depth_frame& frame, pixel u, pixel v)
+{
+    float upixel[2]; // From pixel
+    float upoint[3]; // From point (in 3D)
+
+    float vpixel[2]; // To pixel
+    float vpoint[3]; // To point (in 3D)
+
+    // Copy pixels into the arrays (to match rsutil signatures)
+    upixel[0] = u.first;
+    upixel[1] = u.second;
+    vpixel[0] = v.first;
+    vpixel[1] = v.second;
+
+    // Query the frame for distance
+    // Note: this can be optimized
+    // It is not recommended to issue an API call for each pixel
+    // (since the compiler can't inline these)
+    // However, in this example it is not one of the bottlenecks
+    auto udist = frame.get_distance(upixel[0], upixel[1]);
+    auto vdist = frame.get_distance(vpixel[0], vpixel[1]);
+
+    // Deproject from pixel to point in 3D
+    rs2_intrinsics intr = frame.get_profile().as<rs2::video_stream_profile>().get_intrinsics(); // Calibration data
+    rs2_deproject_pixel_to_point(upoint, &intr, upixel, udist);
+    cout << "upoint: ";
+    display_point(upoint);
+    rs2_deproject_pixel_to_point(vpoint, &intr, vpixel, vdist);
+    cout << "vpoint: ";
+    display_point(vpoint);
+
+    // Calculate euclidean distance between the two points
+    return sqrt(pow(upoint[0] - vpoint[0], 2) +
+                pow(upoint[1] - vpoint[1], 2) +
+                pow(upoint[2] - vpoint[2], 2));
+}
+
+float dist_2d(const pixel& a, const pixel& b)
+{
+    return pow(a.first - b.first, 2) + pow(a.second - b.second, 2);
+}
--- rs/sample/ir.cpp
+++ rs/sample/ir.cpp
@@ -2,18 +2,17 @@
  **  simple opencv + realsense
  **/
 
-#include <iostream>
-// include the librealsense C++ header file
-#include <librealsense2/rs.hpp>
+#include "dist.h"
 
-// include OpenCV header file
-#include <opencv2/opencv.hpp>
+// Neighbors function returns 12 fixed neighboring pixels in an image
+//std::array<pixel, 12> neighbors(rs2::depth_frame frame, pixel p);
 
-using namespace std;
-using namespace cv;
 
 int main()
 {
+    const int WIDTH = 640;
+    const int HEIGHT = 480;
+
     //Contruct a pipeline which abstracts the device
     rs2::pipeline pipe;
 
@@ -21,8 +20,9 @@ int main()
     rs2::config cfg;
 
     //Add desired streams to configuration
-    cfg.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
-    cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
+    //cfg.enable_stream(RS2_STREAM_INFRARED, WIDTH, HEIGHT, RS2_FORMAT_Y8, 30);
+    cfg.enable_stream(RS2_STREAM_COLOR, WIDTH, HEIGHT, RS2_FORMAT_BGR8, 30);
+    cfg.enable_stream(RS2_STREAM_DEPTH, WIDTH, HEIGHT, RS2_FORMAT_Z16, 30);
 
     //Instruct pipeline to start streaming with the requested configuration
     pipe.start(cfg);
@@ -35,23 +35,46 @@ int main()
         frames = pipe.wait_for_frames();
     }
 
-    //Get each frame
-    rs2::frame ir_frame = frames.first(RS2_STREAM_INFRARED);
-    rs2::frame depth_frame = frames.get_depth_frame();
+    const int thickness = 2;
+    const int radius = 6;
+    //const int tail = 5;
+    Scalar white = Scalar(255, 255, 255);
 
-    // Creating OpenCV matrix from IR image
-    Mat ir(Size(640, 480), CV_8UC1, (void*)ir_frame.get_data(), Mat::AUTO_STEP);
+    while (true) {
 
-    // Apply Histogram Equalization
-    equalizeHist( ir, ir );
-    applyColorMap(ir, ir, COLORMAP_JET);
+        frames = pipe.wait_for_frames(); // Wait for next set of frames from the camera
 
-    // Display the image in GUI
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", ir);
+        //rs2::frame ir_frame = frames.first(RS2_STREAM_INFRARED);
+        rs2::frame color_frame = frames.get_color_frame();
+        rs2::frame depth_frame = frames.get_depth_frame();
 
-    cout << "press any key to quit...\n";
-    waitKey(0);
+        // Creating OpenCV matrix from IR image
+        //Mat ir(Size(WIDTH, HEIGHT), CV_8UC1, (void*)ir_frame.get_data(), Mat::AUTO_STEP);
+        Mat color(Size(WIDTH, HEIGHT), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
 
+        // Apply Histogram Equalization
+        //equalizeHist( ir, ir );
+        //applyColorMap(ir, ir, COLORMAP_JET);
+
+        pixel u = {WIDTH/2, HEIGHT/2-100};
+        pixel v = {WIDTH/2, HEIGHT/2};
+        auto d = dist_3d(depth_frame, u, v);
+        cout << "d: " << d << endl;
+
+        circle(color, Point(u.first, u.second), radius, white, thickness);
+        circle(color, Point(v.first, v.second), radius, white, thickness);
+
+        // Display the image in GUI
+        namedWindow("Display Image", WINDOW_AUTOSIZE );
+        imshow("Display Image", color);
+
+        //cout << "press any key to quit...\n";
+        if ( waitKey(1) == 0x1B ) {
+            break;
+        }
+    }
+
+    pipe.stop();
     return 0;
 }
+
