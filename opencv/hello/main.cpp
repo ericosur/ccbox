bec9d694310b8a3afaf6b5c8fab8c3ecca5dd81c
ericosur <ericosur@gmail.com>
2018-11-12 14:10:26 +0800

    UPDATE hello, add example to use opencv LineIterator

--- opencv/hello/main.cpp
+++ opencv/hello/main.cpp
@@ -1,4 +1,8 @@
-#include <opencv2/core.hpp>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
 
 int hello_cv2()
 {
@@ -6,8 +10,45 @@ int hello_cv2()
     return 0;
 }
 
+/// [in] img
+/// [in] p1, p2
+/// [out] points
+int iter(const cv::Mat& img, const cv::Point& p1, const cv::Point& p2, std::vector<cv::Point>& points)
+{
+    LineIterator it(img, p1, p2);
+
+    //cout << "from " << p1 << " to " << p2 << "\t";
+    //cout << "it.count: " << it.count << endl;
+    for (int i = 0; i < it.count; i ++) {
+        // px is the color value of pixel
+        //Vec3b px = Vec3b(*it);
+        //cout << "pos: " << it.pos() << "\t";
+        //cout << "bgr: " << px << endl;
+        points.push_back(it.pos());
+        it++;
+    }
+    return it.count;
+}
+
+void test_iter()
+{
+    Point p1 = Point(50, 5);
+    Point p2 = Point(128, 153);
+    Mat img = imread("../small.png");
+    std::vector<cv::Point> points;
+
+    int ret = iter(img, p1, p2, points);
+    if (ret) {
+        for (size_t i=0; i<points.size(); i++) {
+            cout << points.at(i) << endl;
+        }
+    }
+
+}
+
 int main(int argc, char *argv[])
 {
     hello_cv2();
+    test_iter();
     return 0;
 }
