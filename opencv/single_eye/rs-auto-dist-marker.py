ada790e5dbedae571f3fa2a62563d9e53d95c687
ericosur <ericosur@gmail.com>
2018-12-28 18:50:13 +0800

    UPDATE single_eye: add rs-auto-dist-marker and change output of rs-est-dist.py
    rs-auto-dist-marker will find the marker and calulate actual
    width of upper horizontal line of bounding box with realsense.
    
    It has large error, need to be clarified.

--- opencv/single_eye/rs-auto-dist-marker.py
+++ opencv/single_eye/rs-auto-dist-marker.py
@@ -8,7 +8,7 @@ import numpy as np
 import cv2
 import pyrealsense2 as rs
 
-# 找到目标函数
+# find marker
 def find_marker(image):
     # convert the image to grayscale, blur it, and detect edges
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -26,7 +26,7 @@ def find_marker(image):
     # rect[1][0]是width，rect[1][1]是height，rect[2]是角度
     return cv2.minAreaRect(c)
 
-# 距离计算函数
+# get distance from known width and focal length
 def distance_to_camera(knownWidth, focalLength, perWidth):
     # compute and return the distance from the maker to the camera
     return (knownWidth * focalLength) / perWidth
@@ -36,8 +36,11 @@ def draw_bounding_box(frame, marker):
     # draw a bounding box around the image and display it
     #box = np.int0(cv2.cv.BoxPoints(marker))
     box = cv2.boxPoints(marker)
+    #print('box:{} {}'.format(type(box), box))
     box = np.int0(box)
+    #print('box: {}'.format(box))
     cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
+
 
 
 def main():
@@ -51,29 +54,21 @@ def main():
     KNOWN_HEIGHT = 201
     window_name = 'capture'
 
-    # initialize the list of images that we'll be using
-    IMAGE_PATHS = ["pic0001.png", "pic0002.png", "pic0003.png"]
-
-    # load the furst image that contains an object that is KNOWN TO BE 2 feet
-    # from our camera, then find the paper marker in the image, and initialize
-    # the focal length
-    #读入第一张图，通过已知距离计算相机焦距
-    fn = IMAGE_PATHS[0]
+    '''
     image = cv2.imread(fn)
     marker = find_marker(image)
+    #通过摄像头标定获取的像素焦距
     focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+    print('focalLength = ',focalLength)
     draw_bounding_box(image, marker)
     print('output reference image to focal.png')
     cv2.imwrite('focal.png', image)
-
-    #通过摄像头标定获取的像素焦距
-    #focalLength = 811.82
-    print('focalLength = ',focalLength)
-    #return
+    '''
 
     # Configure depth and color streams
     pipeline = rs.pipeline()
     config = rs.config()
+    # 640x480, 640x360
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
@@ -108,22 +103,52 @@ def main():
                 print(marker)
                 continue
 
-            dist = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+            #dist = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
 
             draw_bounding_box(color_image, marker)
 
+            depth_intrinsics = rs.video_stream_profile(depth_frame.profile).get_intrinsics()
+            # u, v is 2D pixel coordinate
+            # return p is 3D (x, y, z)
+            def get_point(u, v):
+                d = depth_frame.get_distance(u, v)
+                p = rs.rs2_deproject_pixel_to_point(depth_intrinsics, [u, v], d)
+                return p
+
+            def dist_3d(p1, p2):
+                d = math.sqrt(math.pow(p1[0]-p2[0], 2) +
+                              math.pow(p1[1]-p2[1], 2) +
+                              math.pow(p1[2]-p2[2], 2));
+                return d
+
+            def get_marker_xyz(marker):
+                u = int(marker[0][0])
+                v = int(marker[0][1])
+                p0 = get_point(u, v)
+                u = int(marker[1][0])
+                v = int(marker[1][1])
+                p1 = get_point(u, v)
+                d = dist_3d(p0, p1)
+                return d
+
+            d = get_marker_xyz(marker) * 1000
             dist_from_rs = depth_frame.get_distance(int(marker[0][0]), int(marker[0][1]))
             dist_from_rs *= 1000
+
             #print('marker:{} dist_rs:{}'.format(marker, dist_from_rs))
-            cv2.putText(color_image, "%4.0fmm" % dist,
+            # cv2.putText(color_image, "%4.0fmm" % dist,
+            #             (color_image.shape[1] - 500, color_image.shape[0] - 60),
+            #             cv2.FONT_HERSHEY_SIMPLEX,
+            #             1.2, (0, 255, 0), 3)
+
+            cv2.putText(color_image, "p0d: %4dmm" % dist_from_rs,
                         (color_image.shape[1] - 500, color_image.shape[0] - 60),
                         cv2.FONT_HERSHEY_SIMPLEX,
-                        1.2, (0, 255, 0), 3)
-
-            cv2.putText(color_image, "%4dmm" % dist_from_rs,
+                        1.2, (0, 255, 255), 3)
+            cv2.putText(color_image, "w: %4dmm" % d,
                         (color_image.shape[1] - 500, color_image.shape[0] - 20),
                         cv2.FONT_HERSHEY_SIMPLEX,
-                        1.2, (0, 255, 255), 3)
+                        1.2, (0, 255, 127), 3)
 
             # show a frame
             cv2.imshow(window_name, color_image)
