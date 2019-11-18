c6e048f0a3fb26c9004ff5302bded56ac0e0cf26
ericosur <ericosur@gmail.com>
2019-11-18 09:50:08 +0800

    temp work saved

--- rs/python/read_bag_depth.py
+++ rs/python/read_bag_depth.py
@@ -47,7 +47,8 @@ try:
     # Tell config that we will use a recorded device from filem to be used by the pipeline through playback.
     rs.config.enable_device_from_file(config, args.input)
     # Configure the pipeline to stream the depth stream
-    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+    #print('set depth stream with 1280x720, z16')
+    #config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
 
     # Start streaming from file
     pipeline.start(config)
