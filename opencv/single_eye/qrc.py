b87bb4c8a777d0a567f07c84d0a03818680b1326
ericosur <ericosur@gmail.com>
2019-12-09 18:08:58 +0800

    UPDATE qrc.py, pylint'ed, add add_text_to_image()
    symptom:
    CJK characters are rendered as ??? while draw text onto image
    
    root cause:
    Font of cv.putText() is Hershey which is lack of CJK characters.
    
    solution:
    With PIL, we could specify font for drawing text.

--- opencv/single_eye/qrc.py
+++ opencv/single_eye/qrc.py
@@ -1,37 +1,62 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''
+using qrcode detector of opencv to detect and decode QRCode
+add a function that use PIL to draw text on image for CJK characters
+'''
+
+import sys
 import numpy as np
 import cv2 as cv
+from PIL import Image, ImageDraw, ImageFont
 import myutil
 
 
-det = cv.QRCodeDetector()
+# pylint: disable=too-many-arguments
+def add_text_to_image(img, text, left, top, textColor=(0, 255, 0), textSize=20):
+    ''' using PIL to render text '''
+    if isinstance(img, np.ndarray):
+        img = Image.fromarray(cv.cvtColor(img, cv.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img)
+    fontText = ImageFont.truetype(
+        "NotoSansCJK-Regular.ttc", textSize, encoding="utf-8")
+    draw.text((left, top), text, textColor, font=fontText)
+    return cv.cvtColor(np.asarray(img), cv.COLOR_RGB2BGR)
 
 
 def draw_qrcode_contour(img, text, points, win_title='result'):
+    ''' draw qrcode contour '''
     #rgb(66, 134, 244)
     cv.drawContours(img, points, 0, (244, 134, 66), 2)
-    cv.putText(img, text, (10, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (127, 0, 255), 2, cv.LINE_AA)
+    #cv.putText(img, text, (10, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (127, 0, 255),
+    #           2, cv.LINE_AA)
+    img = add_text_to_image(img, text, 10, 10)
     cv.imshow(win_title, img)
 
 
 def run_detect(img):
+    ''' run_detect '''
     disp_img = img.copy()
-
+    det = cv.QRCodeDetector()
     retval, points = det.detect(img)
     if retval:
         print("points: {}".format(points))
-        draw_qrcode_contour(disp_img, [np.array(points, dtype=np.int32)], win_title=run_detect.__name__)
+        draw_qrcode_contour(disp_img, retval, [np.array(points, dtype=np.int32)],
+                            win_title=run_detect.__name__)
     else:
         print('not detected...')
 
 def run_detect_decode(img, webcam=False):
+    ''' run_detect_decode '''
     disp_img = img.copy()
-    retval, points, straight_qrcode = det.detectAndDecode(img)
+    det = cv.QRCodeDetector()
+    #retval, points, straight_qrcode = det.detectAndDecode(img)
+    retval, points, _ = det.detectAndDecode(img)
     if retval:
         print('retval: {}'.format(retval))
-        draw_qrcode_contour(disp_img, retval, [np.array(points, dtype=np.int32)], win_title=run_detect_decode.__name__)
+        draw_qrcode_contour(disp_img, retval, [np.array(points, dtype=np.int32)],
+                            win_title=run_detect_decode.__name__)
     else:
         if not webcam:
             print('no result')
@@ -40,16 +65,18 @@ def run_detect_decode(img, webcam=False):
 
 
 def do_image(fn):
+    ''' qrcode from image '''
     print('read: {}'.format(fn))
     if not myutil.isfile(fn):
         print("image {} not found...".format(fn))
-        exit(1)
+        sys.exit(1)
     img = cv.imread(fn)
     #run_detect(img)
     run_detect_decode(img)
 
 
 def main():
+    ''' main '''
     use_image = False
 
     if use_image:
